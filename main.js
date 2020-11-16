9ca4dd55085aaf6906290de05721cf9ba0e35855
kolyas238 <kolyas238@gmail.com>
2020-11-17 00:35:19 +0300

    добавляет второе задание

--- main.js
+++ main.js
@@ -25,3 +25,31 @@ function letterCounter(firstStr, secondStr) {
 }
 
 console.log(letterCounter(firstRow, secondRow));
+
+/* Второе задание*/
+let num = '+71234567890';
+
+function formattedPhone(phone) {
+  let acc = '';
+  if (phone.length > 12) {
+    return 'Слишком длинный номер'
+  } else if (phone.length < 12) {
+    return 'Слишком короткий номер'
+  } else {
+    for (var i = 0; i < phone.length; i++) {
+      acc += phone[i];
+      if (acc.length == 2) {
+        acc += ' (';
+      } else if (acc.length == 7) {
+        acc += ') ';
+      } else if (acc.length == 12) {
+        acc += '-';
+      } else if (acc.length == 15) {
+        acc += '-';
+      }
+    };
+  }
+  return acc;
+};
+
+console.log(formattedPhone(num));
