8034db4d145f8ca87336029ff3d73854d71c63ad
kolyas238 <kolyas238@gmail.com>
2020-11-22 13:20:42 +0300

    Добавляет текущий урон

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ function renderProgressbarHP(person) {
 
 function changeHP(count, person) {
   this.damageHP -= count;
-  const log = this === enemy ? generateLogs(this, character) : generateLogs(this, enemy);
+  const log = this === enemy ? generateLogs(this, character, -count) : generateLogs(this, enemy, -count);
   getLog(log);
 
   if (this.damageHP <= 0) {
@@ -86,18 +86,18 @@ function random(num) {
   return Math.ceil(Math.random() * num);
 }
 
-function generateLogs(firstPerson, secondPerson) {
+function generateLogs(firstPerson, secondPerson, count) {
   const logs = [
-      `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
       `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
-      `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. [${firstPerson.damageHP}/${firstPerson.defaultHP}]`
+      `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`,
+      `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. ${count} [${firstPerson.damageHP}/${firstPerson.defaultHP}]`
   ];
 
   return logs[random(logs.length) - 1]
